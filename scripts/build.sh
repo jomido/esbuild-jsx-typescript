f4a17da04e91bfb35603fd441cf076576ec43630
Jon Dobson <jon@dobson.land>
2022-05-05 11:05:44 -0400

    move script body's to scripts dir

---
+++ scripts/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# BEGIN SANITY BOX------------------------------------------
+set -e           # immediately exit if any command fails   |
+set -u           # immediately fail on undefined variables |
+set -o pipefail  # show error of last failed command       |
+IFS=$'\n\t'      # control what word splitting means       |
+# END SANITY BOX--------------------------------------------
+
+esbuild app.tsx \
+  --bundle \
+  --inject:./react-shim.js \
+  --jsx-factory=h \
+  --jsx-fragment=Fragment \
+  --outfile=./dist/bundle.js
